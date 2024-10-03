bc386bcb71bf2295aa83f016b5e27c5c250c1c1e
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-10-03 18:03:14 +0530

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import auth from 'auth-astro';
 import icon from 'astro-icon';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from 'astro/config';
+// Import /serverless for a Serverless SSR site
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [tailwind(), react(), icon({}), auth()],
   output: 'server',
-  adapter: netlify(),
+  adapter: vercelServerless(),
 });
