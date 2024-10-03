0d69f9c22de37ea85333c0ae2c23f02882cff727
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-10-03 18:09:30 +0530

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import auth from 'auth-astro';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 // Import /serverless for a Serverless SSR site
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [tailwind(), react(), icon({}), auth()],
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: vercel(),
 });
